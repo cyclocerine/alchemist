d3a42f6c3dc81a85db8435b48789254e92514151
Fa'iq Hammam <hammamfaiq@protonmail.com>
2023-05-29 09:34:44 +0700

    register file added

--- register/mail.php
+++ register/mail.php
@@ -4,7 +4,7 @@ error_reporting( E_ALL );
 $from = "register@alchemist.or.id";
 $to = "hammamfaiq@protonmail.com";
 $subject = "Register User";
-$message = $_POST[PESAN],$_POST[NAMA]:
+$message = $_POST[first_name],$_POST[last_name],$_POST[school],$_POST[email],$_POST[area_code],$_POST[phone],$_POST[subject],$_POST[exist];
 $headers = "From:" . $from;
 mail($to,$subject,$message, $headers);
 echo "The registration request was sent." ;
